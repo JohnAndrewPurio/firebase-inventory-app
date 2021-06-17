a535d25761e5cb6bc1b5bba65a3dbde40bcc1660
John Andrew Purio <lyerdestroyer@gmail.com>
2021-06-17 09:22:57 +0800

    Draft Commit

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,3 +1,4 @@
+import { CssBaseline } from '@material-ui/core'
 import NavBar from '../NavBar'
 import ApplicationBar from '../ApplicationBar'
 import { useSelector } from 'react-redux'
@@ -13,6 +14,7 @@ export default function Home() {
 
     return (
         <div>
+            <CssBaseline />
             <NavBar />
             <AddNewItem />
             <DisplayArea />
